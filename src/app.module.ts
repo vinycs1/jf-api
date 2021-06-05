a6f4fdc8c86a24868b326dda16134c03808cb45f
Vinicius <vinicius.castro@paretogroup.com.br>
2021-06-05 20:11:40 -0300

    add initial configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'mongodb',
+          autoLoadEntities: true,
+          synchronize: true,
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          database: configService.get('DB_DATABASE'),
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/config.schema.ts
@@ -0,0 +1,8 @@
+import * as Joi from '@hapi/joi';
+
+export const configValidationSchema = Joi.object({
+  STAGE: Joi.string().required(),
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().default(27017).required(),
+  DB_DATABASE: Joi.string().required(),
+});
